08a74e5cbcb39deb1881be7b5afbb901ae878c0d
YouYouzmr <595093508@qq.com>
2021-09-10 18:29:07 +0800

    新建项目

---
+++ bin/vuenext-admin-cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+// console.log("vuenext-admin-cli...")
+
+if(process.argv[0] === '-v' || process.argv[0] === '--version') {
+}
+   
+// 定制命令行界面
+const program = require("commander")
+program.version(require('../package.json').version)
+
+// act int vuenext-admin-cli 创建 vuenext-admin-cli 工厂
+program.command('init <name>')
+    .description("init project")
+    .action(require('../lib/init.js'))
+
+// argv 执行中所有参数
+program.parse(process.argv)
